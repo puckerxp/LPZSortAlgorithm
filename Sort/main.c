e774efd61520e5e43023b9dd51b7abf04f5a4c0b
Puzhi Li <puckerxp@hotmail.com>
2017-04-06 19:35:28 +0800

    Add Selection Sort.

--- Sort/main.c
+++ Sort/main.c
@@ -10,17 +10,43 @@
 
 void Print(int *p, int n);
 
-void Swap(int *a, int *b)
+void Swap(int p[], int i, int j)
 {
-    printf("%s %d %d\n", __FUNCTION__, *a, *b);
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+    printf("%s %d %d\n", __FUNCTION__, p[i], p[j]);
+    int temp = p[i];
+    p[i] = p[j];
+    p[j] = temp;
+}
+
+void SelectionSort(int p[], int n)
+{
+    printf("%s\n", __FUNCTION__);
+    int minIndex;
+    for (int i = 0; i < n; ++i)
+    {
+        minIndex = i;
+
+        for (int j = i + 1; j < n; ++j)
+        {
+            if (p[minIndex] > p[j])
+            {
+                minIndex = j;
+            }
+        }
+
+        if (minIndex != i)
+        {
+            Swap(p, i, minIndex);
+        }
+
+        printf("Round %d\n", i);
+        Print(p, n);
+    }
 }
 
 void OptimizedBubbleSort(int p[], int n)
 {
-    printf("\n%s\n", __FUNCTION__);
+    printf("%s\n", __FUNCTION__);
     int alreadySorted = 1;
     for (int i = 0; i < n - 1 && alreadySorted; ++i)
     {
@@ -29,7 +55,7 @@ void OptimizedBubbleSort(int p[], int n)
         {
             if (p[j - 1] > p[j])
             {
-                Swap(p + j - 1, p + j);
+                Swap(p, j - 1, j);
                 alreadySorted = 1;
             }
         }
@@ -41,14 +67,14 @@ void OptimizedBubbleSort(int p[], int n)
 
 void BubbleSort(int p[], int n)
 {
-    printf("\n%s\n", __FUNCTION__);
+    printf("%s\n", __FUNCTION__);
     for (int i = 0; i < n - 1; ++i)
     {
         for (int j = n - 1; j > i; --j)
         {
             if (p[j - 1] > p[j])
             {
-                Swap(p + j - 1, p + j);
+                Swap(p, j - 1, j);
             }
         }
 
@@ -66,6 +92,10 @@ int main(int argc, const char *argv[])
     int b[10] = {34, 18, 76, 37, 96, 45, 82, 23, 53, 68};
     Print(b, 10);
     OptimizedBubbleSort(b, 10);
+
+    int c[10] = {34, 18, 76, 37, 96, 45, 82, 23, 53, 68};
+    Print(c, 10);
+    SelectionSort(c, 10);
 
     return 0;
 }
