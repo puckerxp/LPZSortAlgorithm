680c13e821ea34049cc285c22053f83dbe7410d4
Puzhi Li <puckerxp@hotmail.com>
2017-04-06 19:17:44 +0800

    Add an optimized Bubble Sort.

--- Sort/main.c
+++ Sort/main.c
@@ -12,14 +12,36 @@ void Print(int *p, int n);
 
 void Swap(int *a, int *b)
 {
-    printf("%s\n", __FUNCTION__);
+    printf("%s %d %d\n", __FUNCTION__, *a, *b);
     int temp = *a;
     *a = *b;
     *b = temp;
 }
 
+void OptimizedBubbleSort(int p[], int n)
+{
+    printf("\n%s\n", __FUNCTION__);
+    int alreadySorted = 1;
+    for (int i = 0; i < n - 1 && alreadySorted; ++i)
+    {
+        alreadySorted = 0;
+        for (int j = n - 1; j > i; --j)
+        {
+            if (p[j - 1] > p[j])
+            {
+                Swap(p + j - 1, p + j);
+                alreadySorted = 1;
+            }
+        }
+
+        printf("Round %d\n", i);
+        Print(p, n);
+    }
+}
+
 void BubbleSort(int p[], int n)
 {
+    printf("\n%s\n", __FUNCTION__);
     for (int i = 0; i < n - 1; ++i)
     {
         for (int j = n - 1; j > i; --j)
@@ -32,22 +54,19 @@ void BubbleSort(int p[], int n)
 
         printf("Round %d\n", i);
         Print(p, n);
-
     }
 }
 
 int main(int argc, const char *argv[])
 {
-    // insert code here...
-    printf("Hello, World!\n");
+    int a[10] = {34, 18, 76, 37, 96, 45, 82, 23, 53, 68};
+    Print(a, 10);
+    BubbleSort(a, 10);
 
-    int p[10] = {34, 18, 76, 37, 96, 45, 82, 23, 53, 68};
+    int b[10] = {34, 18, 76, 37, 96, 45, 82, 23, 53, 68};
+    Print(b, 10);
+    OptimizedBubbleSort(b, 10);
 
-    Print(p, 10);
-
-    BubbleSort(p, 10);
-
-    Print(p, 10);
     return 0;
 }
 
